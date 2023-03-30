9c78e92525c9be5e067f9ef14205543135aa3543
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-03-30 21:52:39 +0530

    dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,10 @@
-
-import Header from './components/Header'
-import Singlepost from './components/Singlepost';
-import {Route,Routes} from 'react-router-dom'
-import './index.css';
-import Layout from './components/Layout';
-import IndexPage from './pages/IndexPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+const { default: NavBar } = require("./components/NavBar");
 
 function App() {
   return (
-    
-    <Routes>
-      <Route path='/' element={<Layout />}>
-      <Route index element={
-       <IndexPage />
-      }/>
-
-      <Route path={'/login'} element={
-        <LoginPage />
-      }/>
-      <Route path='/register' element={<RegisterPage />} />
-      </Route>
-    </Routes>
-    
+    <>
+    <NavBar />
+    </>
   );
 }
 
