272b271a5487dabb94a750715650605918727eaa
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-04-02 01:52:01 +0530

    staged

--- server/api/logout.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-router.post('/', (req, res)=>{
-    res.cookie('token', '').json("Looged Out")
-})
-
-module.exports = router
--- server/middleware/verifyJWT.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const verifyJWT = (req, res, next) => {
-    const authHeaders = req.headers['authorization']
-
-    if(!authHeaders) return res.sendStatus(401)
-
-    
-}
