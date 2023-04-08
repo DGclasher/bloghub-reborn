1ce450dd35ea241ea362418e93683f05ced76dc3
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-04-09 00:33:57 +0530

    dev

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 
 mongoose
   .connect(process.env.MONGO_URL, {
