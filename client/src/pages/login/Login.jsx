fc1ab20abd86905d5027331cd08f85257fc55dc4
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-04-08 23:50:53 +0530

    client

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -4,44 +4,72 @@ import axios from "axios";
 import { Context } from "../../context/Context";
 
 export default function Login() {
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
 
-  const userRef = useRef()
-  const passwordRef = useRef()
-  const {dispatch, isFetching, user} = useContext(Context)
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching, user } = useContext(Context);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    dispatch({type: "LOGIN_START"})
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", {
-        username: userRef.current.value,
-        password: passwordRef.current.value
-      })
-      dispatch({type: "LOGIN_SUCCESS", payload: res.data})
-      window.location.replace("/")
+      const res = await axios.post(
+        "/auth/login",
+        {
+          username: userRef.current.value,
+          password: passwordRef.current.value,
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      window.location.replace("/");
     } catch (error) {
-      dispatch({type: "LOGIN_FAILURE"})
-      setError(true)
-    }    
-  }
-  console.log(user)
+      dispatch({ type: "LOGIN_FAILURE" });
+      setError(true);
+    }
+  };
+  console.log(user);
   return (
     <div className="items-center justify-center flex pt-8 text-white">
-
-    <div className="items-center flex flex-col justify-center bg-zinc-900 w-[350px] md:w-[500px] h-[450px] rounded-xl">
-      <span className="text-4xl pb-3">Login</span>
-      <form className="gap-y-3 flex flex-col items-center" onSubmit={handleSubmit}>
-        <label className="text-lg">Username</label>
-        <input className="p-3 rounded-lg text-white bg-zinc-700" type="text" placeholder="Enter your username..." ref={userRef} />
-        <label className="text-lg">Password</label>
-        <input className="p-3 rounded-lg text-white bg-zinc-700" type="password" placeholder="Enter your password..." ref={passwordRef}/>
-        <button className="p-2 mt-3 rounded-lg bg-lime-600 hover:bg-lime-800 text-black cursor-pointer" type="submit" disabled={isFetching}>Login</button>
-      </form>
-      {error && (
-        <span style={{color: "red", marginTop: "10px"}}>Wrong credentials!</span>
+      <div className="items-center flex flex-col justify-center bg-zinc-900 w-[350px] md:w-[500px] h-[450px] rounded-xl">
+        <span className="text-4xl pb-3">Login</span>
+        <form
+          className="gap-y-3 flex flex-col items-center"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-lg">Username</label>
+          <input
+            className="p-3 rounded-lg text-white bg-zinc-700"
+            type="text"
+            placeholder="Enter your username..."
+            ref={userRef}
+          />
+          <label className="text-lg">Password</label>
+          <input
+            className="p-3 rounded-lg text-white bg-zinc-700"
+            type="password"
+            placeholder="Enter your password..."
+            ref={passwordRef}
+          />
+          <button
+            className="p-2 mt-3 rounded-lg bg-lime-600 hover:bg-lime-800 text-black cursor-pointer"
+            type="submit"
+            disabled={isFetching}
+          >
+            Login
+          </button>
+        </form>
+        {error && (
+          <span style={{ color: "red", marginTop: "10px" }}>
+            Wrong credentials!
+          </span>
         )}
-    </div>
       </div>
+    </div>
   );
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,47 +1,78 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./register.css"
-import axios from "axios"
+import "./register.css";
+import axios from "axios";
 
 export default function Register() {
-  const [username, setUsernmae] = useState([])
-  const [password, setPassword] = useState([])
-  const [email, setEmail] = useState([])
-  const [error, setError] = useState(false)
+  const [username, setUsernmae] = useState([]);
+  const [password, setPassword] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [error, setError] = useState(false);
 
-  const handleSubmit =async (e) => {
-    e.preventDefault()
-    setError(false)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
     try {
-      const res = await axios.post("/auth/register", { 
-        username,
-        email,
-        password
-      })
-      res.data && window.location.replace("/login")
+      const res = await axios.post(
+        "/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+      res.data && window.location.replace("/login");
     } catch (error) {
-      setError(true)
+      setError(true);
     }
-    
-  }
-    return (
-      <div className="items-center justify-center flex pt-8 text-white">
-
-    <div className="items-center flex flex-col justify-center bg-zinc-900 w-[350px] md:w-[500px] h-[500px] rounded-xl">
-      <span className="text-4xl pb-4">Register</span>
-      <form className="gap-y-3 flex flex-col items-center" onSubmit={handleSubmit}>
-        <label className="text-lg">Username</label>
-        <input className="p-3 rounded-lg text-white bg-zinc-700" type="text" placeholder="Enter your username..." onClick={e=>setUsernmae(e.target.value)} />
-        <label className="text-lg">Email</label>
-        <input className="p-3 rounded-lg text-white bg-zinc-700" type="email" placeholder="Enter your email..." onClick={e=>setEmail(e.target.value)}/>
-        <label className="text-lg">Password</label>
-        <input className="p-3 rounded-lg text-white bg-zinc-700" type="password" placeholder="Enter your password..." onClick={e=>setPassword(e.target.value)}/>
-        <button className="p-2 mt-3 rounded-lg bg-lime-600 hover:bg-lime-800 text-black cursor-pointer" type="submit">Register</button>
-      </form>
-      {error && (
-        <span style={{color: "red", marginTop: "10px"}}>Wrong credentials!</span>
+  };
+  return (
+    <div className="items-center justify-center flex pt-8 text-white">
+      <div className="items-center flex flex-col justify-center bg-zinc-900 w-[350px] md:w-[500px] h-[500px] rounded-xl">
+        <span className="text-4xl pb-4">Register</span>
+        <form
+          className="gap-y-3 flex flex-col items-center"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-lg">Username</label>
+          <input
+            className="p-3 rounded-lg text-white bg-zinc-700"
+            type="text"
+            placeholder="Enter your username..."
+            onClick={(e) => setUsernmae(e.target.value)}
+          />
+          <label className="text-lg">Email</label>
+          <input
+            className="p-3 rounded-lg text-white bg-zinc-700"
+            type="email"
+            placeholder="Enter your email..."
+            onClick={(e) => setEmail(e.target.value)}
+          />
+          <label className="text-lg">Password</label>
+          <input
+            className="p-3 rounded-lg text-white bg-zinc-700"
+            type="password"
+            placeholder="Enter your password..."
+            onClick={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="p-2 mt-3 rounded-lg bg-lime-600 hover:bg-lime-800 text-black cursor-pointer"
+            type="submit"
+          >
+            Register
+          </button>
+        </form>
+        {error && (
+          <span style={{ color: "red", marginTop: "10px" }}>
+            Wrong credentials!
+          </span>
         )}
-    </div>
       </div>
-    )
+    </div>
+  );
 }
