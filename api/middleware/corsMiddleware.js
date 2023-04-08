ef6e48a17e88175898b8f1e1ae9449594ca3e794
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-04-09 01:36:08 +0530

    main

--- api/middleware/corsMiddleware.js
+++ api/middleware/corsMiddleware.js
@@ -2,6 +2,7 @@ const corsMiddleware = (req, res, next) => {
     // CORS headers
     res.header("Access-Control-Allow-Origin", "https://bloghub-reborn.netlify.app");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header('Access-Control-Allow-Credentials', true);
     // Set custom headers for CORS
     res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
 
